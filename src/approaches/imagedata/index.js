9b7b6fe7d4efe3a57200a7083f5391fa0bd7090a
JuanIrache <yrache@gmail.com>
2021-05-09 23:04:52 +0200

    add imagedata approach (results are broken for now)

---
+++ src/approaches/imagedata/index.js
@@ -0,0 +1,17 @@
+// Use ctx.getImageData to send the raw image data quickly to ffmpeg
+
+module.exports = {
+  ffmpegArgs: (w, h) => [
+    '-f',
+    'rawvideo',
+    '-pix_fmt',
+    'rgba',
+    '-s',
+    `${w}x${h}`
+  ],
+  canvasToFrame: canvas =>
+    canvas
+      .getContext('2d')
+      .getImageData(0, 0, canvas.width, canvas.height)
+      .data.toString()
+};
